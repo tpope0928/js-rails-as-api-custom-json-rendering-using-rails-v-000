31846a6610a7a1405ce349bb46a4409ff872c8aa
Thomas Pope <tpope0928@gmail.com>
2020-04-10 23:52:25 +0000

    Automatically backed up by Learn

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,11 @@
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
-    render json: @birds
+    birds = Bird.all
+    render json: birds
+  end
+
+  def show
+    bird = Bird.find_by(id: params[:id])
+    render json: bird
   end
 end
